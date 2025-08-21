ed2c4dbc51482d2424f3945f6042a5584048b82a
nox <noxabellus@gmail.com>
2025-08-20 23:01:38 -0500

    better basics

--- src/main.zig
+++ src/main.zig
@@ -26,6 +26,11 @@ const uvec4 = math.uvec4;
 const quat = math.quat;
 const mat4 = math.mat4;
 
+const font_size: f32 = 16;
+const gui_font_size: f32 = font_size * 0.6;
+const margin: f32 = 16;
+const half_margin: f32 = 8;
+
 pub fn main() anyerror!void {
     rl.setConfigFlags(.{
         .window_resizable = true,
@@ -36,7 +41,7 @@ pub fn main() anyerror!void {
 
     defer rl.closeWindow();
 
-    var buffer: [256]u8 = undefined;
+    var tts_buffer: [256]u8 = undefined;
 
     var run_timer = try std.time.Timer.start();
     var frame_timer = try std.time.Timer.start();
@@ -44,6 +49,13 @@ pub fn main() anyerror!void {
     const frame_time_avg_count = 100;
     var frame_times = [1]f64{0} ** frame_time_avg_count;
     var frame_time_index: u64 = 0;
+    const font = try rl.loadFontEx("fonts/roboto/regular.ttf", @intFromFloat(font_size), null);
+    defer rl.unloadFont(font);
+
+    const gui_font = try rl.loadFontEx("fonts/roboto/regular.ttf", @intFromFloat(gui_font_size), null);
+    defer rl.unloadFont(gui_font);
+
+    rg.setFont(gui_font);
 
     const shader = try rl.loadShader(null, "shaders/raymarch.fs");
     defer rl.unloadShader(shader);
@@ -51,8 +63,15 @@ pub fn main() anyerror!void {
     const loc_resolution = rl.getShaderLocation(shader, "uResolution");
     const loc_time = rl.getShaderLocation(shader, "uTime");
 
-    var width: i32 = @divFloor(rl.getScreenWidth(), 3);
-    var height: i32 = @divFloor(rl.getScreenHeight(), 3);
+    var scale_factor: f32 = 1.0 / 3.0;
+
+    var width: i32 = @intFromFloat(@ceil(@as(f32, @floatFromInt(rl.getScreenWidth())) * scale_factor));
+    var height: i32 = @intFromFloat(@ceil(@as(f32, @floatFromInt(rl.getScreenHeight())) * scale_factor));
+
+    var selected_fps_limit: f32 = 144;
+    var active_fps_limit: i32 = @intFromFloat(selected_fps_limit);
+
+    rl.setTargetFPS(active_fps_limit);
 
     var target = try rl.loadRenderTexture(width, height);
     defer rl.unloadRenderTexture(target);
@@ -73,8 +92,17 @@ pub fn main() anyerror!void {
         const sw = rl.getScreenWidth();
         const sh = rl.getScreenHeight();
 
-        const cw = @divFloor(sw, 3);
-        const ch = @divFloor(sh, 3);
+        const cw: i32 = @intFromFloat(@ceil(@as(f32, @floatFromInt(sw)) * scale_factor));
+        const ch: i32 = @intFromFloat(@ceil(@as(f32, @floatFromInt(sh)) * scale_factor));
+
+        // ==== FPS Limiter ====
+        {
+            const new_limit: i32 = @intFromFloat(selected_fps_limit);
+            if (active_fps_limit != new_limit) {
+                active_fps_limit = new_limit;
+                rl.setTargetFPS(active_fps_limit);
+            }
+        }
 
         // ==== Raymarcher -> RenderTarget draw ====
         {
@@ -130,15 +158,104 @@ pub fn main() anyerror!void {
 
             // ==== GUI ====
             {
-                // show statistics
-                const text = try std.fmt.bufPrintZ(
-                    &buffer,
-                    "Resolution: {} x {}\n(Avg. {} frames)\n  MS: {d:.3}\n  FPS: {d:.1}",
-                    .{ cw, ch, frame_time_avg_count, frame_time_avg, fps },
-                );
-                const text_width = rl.measureText(text, 20);
-                rl.drawRectangle(5, 5, text_width + 10, 100, .light_gray);
-                rl.drawText(text, 10, 10, 20, .black);
+                var fba = std.heap.FixedBufferAllocator.init(&tts_buffer);
+                const allocator = fba.allocator();
+
+                const lines = [_][:0]const u8{
+                    try std.fmt.allocPrintZ(allocator, "Screen Size: {} x {}", .{ sw, sh }),
+                    try std.fmt.allocPrintZ(allocator, "Ray Resolution: {} x {}", .{ cw, ch }),
+                    try std.fmt.allocPrintZ(allocator, "Avg. {} frames", .{frame_time_avg_count}),
+                    try std.fmt.allocPrintZ(allocator, "  MS : {d:.3}", .{frame_time_avg}),
+                    try std.fmt.allocPrintZ(allocator, "  FPS: {d:.1}", .{fps}),
+                    try std.fmt.allocPrintZ(allocator, "Scale Factor: {d:.2}", .{scale_factor}),
+                    try std.fmt.allocPrintZ(allocator, "Limit FPS: {}", .{active_fps_limit}),
+                };
+
+                const line_indices = .{
+                    .screen_size = 0,
+                    .ray_resolution = 1,
+                    .avg_frames = 2,
+                    .avg_ms = 3,
+                    .avg_fps = 4,
+                    .scale_factor = 5,
+                    .fps_limit = 6,
+                };
+
+                var widths = [_]f32{0} ** lines.len;
+
+                var max_width: f32 = 0;
+                for (lines, 0..) |l, i| {
+                    widths[i] = @floatFromInt(rl.measureText(l, font_size));
+                    max_width = @max(max_width, widths[i]);
+                }
+
+                var box_width: f32 = 100;
+
+                while (box_width < max_width + margin * 2) box_width += 50;
+
+                rl.drawRectangleRec(.{
+                    .x = half_margin,
+                    .y = half_margin,
+                    .width = box_width + margin * 2,
+                    .height = font_size * (lines.len + 1) + margin * 2,
+                }, .light_gray);
+
+                var line_offset: f32 = 0;
+
+                for (lines, 0..) |l, i| {
+                    rl.drawTextPro(
+                        font,
+                        l,
+                        .{ .x = margin, .y = margin + line_offset * font_size },
+                        .{ .x = 0, .y = 0 },
+                        0,
+                        font_size,
+                        1.0,
+                        .black,
+                    );
+
+                    switch (i) {
+                        line_indices.scale_factor => {
+                            line_offset += 1;
+
+                            _ = rg.sliderBar(
+                                .{
+                                    .x = margin,
+                                    .y = margin + line_offset * font_size,
+                                    .width = box_width - margin * 2,
+                                    .height = font_size,
+                                },
+                                "",
+                                "",
+                                &scale_factor,
+                                0.1,
+                                2.0,
+                            );
+                        },
+
+                        line_indices.fps_limit => {
+                            line_offset += 1;
+
+                            _ = rg.sliderBar(
+                                .{
+                                    .x = margin,
+                                    .y = margin + line_offset * font_size,
+                                    .width = box_width - margin * 2,
+                                    .height = font_size,
+                                },
+                                "",
+                                "",
+                                &selected_fps_limit,
+                                0,
+                                240,
+                            );
+                        },
+
+                        else => {},
+                    }
+
+                    line_offset += 1;
+                }
             }
         }
     }
